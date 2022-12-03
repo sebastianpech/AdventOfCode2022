a0f00f8129694266d5f0b0cb56d8834663ae9d99
Sebastian Pech <sebastian.pech@me.com>
2022-12-03 09:22:44 +0100

    Change to mapreduce

--- 3.jl
+++ 3.jl
@@ -1,11 +1,11 @@
 item_priority(c) = isuppercase(c) ?  Int(c)-38 : Int(c)-96
 
 # Part 1
-sum(map(readlines("3.input")) do line
+mapreduce(+,readlines("3.input")) do line
     Iterators.partition(line,length(line)÷2) |> collect .|> Set |> (x->∩(x...)) |> only |> item_priority
-end)
+end
 
 # Part 2
-sum(map(Iterators.partition(readlines("3.input"),3)) do (a,b,c)
+mapreduce(+,Iterators.partition(readlines("3.input"),3)) do (a,b,c)
     item_priority(only(a ∩ b ∩ c))
-end)
+end
